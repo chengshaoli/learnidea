9a8956e3e5741e21ea52230ebcc7e01ff2a8d3f8
Rabcheng <16634010588@163.com>
2020-02-27 15:03:42 +0800

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * @auther cheng
+ * @create 2020-02-27 13:22
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+    final int x = 1;
+
+    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!!");
+
+        ArrayList<String> s = new ArrayList<>();
+    }
+}
